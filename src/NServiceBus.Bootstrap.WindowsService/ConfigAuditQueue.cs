25fcb2256b1b195c627fda72160d7e05efa5006a
Simon Cropp <simon.cropp@gmail.com>
2015-05-20 11:50:00 +1000

    add audit config

---
+++ src/NServiceBus.Bootstrap.WindowsService/ConfigAuditQueue.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+class ConfigAuditQueue : IProvideConfiguration<AuditConfig>
+{
+    public AuditConfig GetConfiguration()
+    {
+        //TODO: optionally choose a different audit queue. Perhaps on a remote machine
+        return new AuditConfig
+               {
+                   QueueName = "audit"
+               };
+    }
+}
